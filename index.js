681b8781e89d2b768cb3c28e3afc47092abf0abc
MrJanHorak <janny_b_goode@hotmail.com>
2022-12-27 23:52:23 -0500

    setup mongoDB connection

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const { application } = require('express');
+const mongoose = require('mongoose');
 const app = express();
-const port = 3000;
+const mongoDB = process.env.MONGO_DB;
+const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.set('strictQuery', true);
+mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 app.get('/', (rwq, res) => {
   res.send('Hello World!');
