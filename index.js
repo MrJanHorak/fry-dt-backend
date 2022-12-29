6d4c8a1c12f54707c533ff7398de3d7434ca777d
MrJanHorak <janny_b_goode@hotmail.com>
2022-12-29 15:28:04 -0500

    update index.js to include and use routes

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ import cors from 'cors';
 // connect to MondgoDB with mongoose
 import('./config/database.js');
 
+// import routes
+import { router as authRouter } from './routes/auth.js';
+import { router as profilesRouter } from './routes/profiles.js';
+
 // create the express app
 const app = express();
 const port = process.env.PORT;
@@ -21,6 +25,19 @@ app.use(
 );
 app.use;
 app.use(bodyParser.json());
+
+// router middleware
+app.use('api/auth', authRouter);
+app.use('api/profiles', profilesRouter);
+
+// catch 404
+app.use(function (req, res, next) {
+  res.status(404).json({ err: 'Not found!' });
+});
+
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500).json({ err: err.message });
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
