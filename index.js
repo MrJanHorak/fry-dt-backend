dc9257555d28debd746ac49c2012636f9e33230e
MrJanHorak <janny_b_goode@hotmail.com>
2022-12-28 22:33:36 -0500

    add MongoDB cloud connection and refactor code

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
-const dotenv = require('dotenv').config();
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const { application } = require('express');
-const mongoose = require('mongoose');
+import 'dotenv/config.js';
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+
+// connect to MondgoDB with mongoose
+import('./config/database.js');
+
+// create the express app
 const app = express();
-const mongoDB = process.env.MONGO_DB;
 const port = process.env.PORT;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: '*',
+    optionsSuccessStatus: 200,
+    methods: 'GET, PUT, POST',
+  })
+);
+app.use;
 app.use(bodyParser.json());
 
-mongoose.set('strictQuery', true);
-mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
-
-const db = mongoose.connection;
-
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-
-app.get('/', (rwq, res) => {
+app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
