8c2e101c0ccb13eb10e723ecfe39c50a13fcada7
MrJanHorak <janny_b_goode@hotmail.com>
2022-12-28 23:14:43 -0500

    create auth middleware

---
+++ middleware/auth.js
@@ -0,0 +1,31 @@
+import jwt from 'jsonwebtoken';
+
+const SECRET = process.env.SECRET;
+
+const decodeUserFromToken = (req, res, next) => {
+  let token = req.get('Authorization') || req.query.token || req.body.token;
+  if (token) {
+    token = token.replace('Bearer ', '');
+    jwt.verify(token, SECRET, (err, decoded) => {
+      if (err) {
+        next(err);
+      } else {
+        req.user = decoded.user;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+};
+
+function checkAuth(req, res, next) {
+  return req.user ? next() : res.status(401).json({ msg: 'Not Authorized' });
+}
+
+function passUserToView(req, res, next) {
+  res.locals.user = req.user ? req.user : null;
+  next();
+}
+
+export { decodeUserFromToken, checkAuth, passUserToView };
