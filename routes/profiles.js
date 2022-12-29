9af65f60b4c45899f36226f968d5572e90d24ae0
MrJanHorak <janny_b_goode@hotmail.com>
2022-12-29 15:19:36 -0500

    add profiles.js routes

---
+++ routes/profiles.js
@@ -0,0 +1,20 @@
+import { Router } from 'express';
+import * as profilesCtrl from '../controllers/profiles.js';
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js';
+
+const router = Router();
+
+/*------- Protected Routes -------*/
+
+router.use(decodeUserFromToken);
+router.get('/', checkAuth, profilesCtrl.index);
+router.get('/:id', checkAuth, profilesCtrl.show);
+router.get('/:id', checkAuth, profilesCtrl.update);
+router.post('/:id/practicedWords', checkAuth, profilesCtrl.addPracticedWord);
+router.put(
+  '/:id/practicedWords/:practicedWordId',
+  checkAuth,
+  profilesCtrl.updatePracticedWord
+);
+
+export { router };
