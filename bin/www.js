062aa99d34d972f175871f19fb26b067982132a8
MrJanHorak <janny_b_goode@hotmail.com>
2023-08-27 15:30:24 -0400

    add send status funtion

--- bin/www.js
+++ bin/www.js
@@ -58,7 +58,7 @@ io.on('connection', (socket) => {
     })
     // Save the new user to the room
     chatRoom = room
-    allUsers.push({ id: socket.id, username, room , user})
+    allUsers.push({ id: socket.id, username, room, user })
     let chatRoomUsers = allUsers.filter((user) => user.room === room)
     socket.to(room).emit('chatroom_users', chatRoomUsers)
     socket.emit('chatroom_users', chatRoomUsers)
@@ -73,11 +73,12 @@ io.on('connection', (socket) => {
   })
 
   socket.on('send_message', (data) => {
-    const { message, username, room, userRole,  __createdtime__ } = data
+    const { message, username, room, userRole, __createdtime__ } = data
     io.in(room).emit('receive_message', data) // Send to all users in room, including sender
-    //   harperSaveMessage(message, username, room, __createdtime__) // Save message in db
-    // .then((response) => console.log(response))
-    // .catch((err) => console.log(err));
+  })
+  socket.on('send_status', (data) => {
+    const { message, username, room, userRole, __createdtime__ } = data
+    io.in(room).emit('receive_status', data) // Send to all users in room, including sender
   })
 
   socket.on('leave_room', (data) => {
